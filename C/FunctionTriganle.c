e886073f0aab493a7334e7acb633c7e90b88489e
Ray <wangleiuu@gmail.com>
2020-12-07 01:48:28 +0800

    2020.12.7

---
+++ C/FunctionTriganle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void print_triangle();
+void print_stars();
+int main()
+{
+   int N;
+   scanf("%d", &N);
+   print_triangle(N);
+   return 0;
+}
+void print_triangle(int N) //打印行
+{
+   for (int i = 1; i <= N; i++)
+   {
+      print_stars(i);
+      printf("\n");
+   }
+}
+void print_stars(int i)    //打印五角星
+{
+   for (int j = 1; j <= i; j++)
+   {
+      printf("*");
+   }
+}
