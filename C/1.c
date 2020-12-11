bb933045a89c969ada6c214a0dcbe89d77d9b4af
unknown <wangleiuu@gmail.com>
2020-12-12 00:00:58 +0800

    2020.12.12

--- C/1.c
+++ C/1.c
@@ -1,24 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-int main()
-{
-    srand(time(0));
-    int counter[11] = {0};
+int main() {
+    int var[5] = {0};
+    printf("请依次输入5个数：");
+    for (int i = 1; i <= 5; i++) {
+        scanf("%d", &var[i - 1]);
+        printf("%d:%d\n", i, var[i - 1]);
+    }
     int n;
-    int i;
-    
     scanf("%d", &n);
-    for (i = 1; i <= n; i++)
-    {
-        int dice1 = rand() % 6 + 1;
-        int dice2 = rand() % 6 + 1;
-        int sum = dice1 + dice2;
-        counter[sum - 2]++;
-    }
-    for (i = 2; i <= 12; i++)
-    {
-        printf("%d: %d\n", i, counter[i - 2]);
-    }
+    printf("Q: %d", var[n - 1]);
     return 0;
 }
--- C/Triangle.c
+++ C/Triangle.c
@@ -1,25 +1,17 @@
-//实心三角形
 #include <stdio.h>
 #include <stdlib.h>
-int main()
-{
+int main() {
     int i, j, height;
-    printf("请问需要几格高的三角形： ");
+    printf("几个高: ");
     scanf("%d", &height);
-    if (height == 0 || height == 1)    //限制最少2格
-    {
+    if (height == 0 || height == 1) {
         printf("至少2格哦!\n");
-    }
-    else
-    {
-        for (int i = 1; i <= height; i++)
-        {
-            for (int j = 1; j <= height - i; j++)    //定义每行几个空格
-            {
+    } else {
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= height - i; j++) {
                 printf(" ");
             }
-            for (int j = 1; j <= 2 * i - 1; j++)    //定义每行几个*
-            {
+            for (int j = 1; j <= 2 * i - 1; j++) {
                 printf("*");
             }
             printf("\n");
