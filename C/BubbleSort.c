76a37382a719aff8549b33e74f8145dcf4bdc10d
unknown <wangleiuu@gmail.com>
2020-12-13 04:10:47 +0800

    Create BubbleSort.c

---
+++ C/BubbleSort.c
@@ -0,0 +1,21 @@
+//泡沫排序
+#include <stdio.h>
+int main(){
+    int i, j, v[5];
+    for ( i = 0; i < 5; i++){
+        scanf("%d", &v[i]);
+    }
+    for (i = 0; i < 5; i++){
+        for (j = 0; j < 5 - i; j++){
+            if (v[j] > v[j + 1]){
+                int t = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = t;
+            }
+        }
+    }
+    for (i = 0; i < 5; i++){
+        printf("%d", v[i]);
+    }
+
+}
